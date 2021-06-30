7c56433a30cba978e2d38d7fc4f3dc7add7c36df
Igor Marchenko <igor.ms83@gmail.com>
2021-07-01 01:13:13 +0300

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-demo-project';
+
+
+  posts: Post[] = [
+    {
+      title: 'Хочу выучить Angular компоненты',
+      text: 'Я все еще учу компоненты',
+      id: 1,
+    },
+    {
+      title: 'Следующий блок',
+      text: 'Будет про директивы и еще про пайпы',
+      id: 2,
+    },
+  ];
+
+  updatePosts(post: Post):void {
+    this.posts.unshift(post)
+    console.log('post', post );
+  }
 }
